38ed7525e99e7edf69772a47e5a93f9dceaa9cb1
Andre Sarmento <alsobox@hotmail.com>
2020-01-18 21:21:18 -0300

    Criando a base da aplicação

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+
+// Query Params: req.query (Filtros, ordenação, paginação, ...)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para criação ou alteração de um registro)
+
+app.post('/users', (request, response) => {
+    console.log(request.body);
+    return response.json({ message: 'Hello Suck' });
+});
+
+app.listen(3333);
